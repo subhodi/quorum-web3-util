6f19c054c599b83e389ccbf1096e6a33c1e2e079
subhodi <subhodpoojary@gmail.com>
2017-12-02 18:34:54 +0530

    Adding Contract config file

---
+++ contract-config.js
@@ -0,0 +1,13 @@
+var config = {
+    doNothing: {
+        name: 'DoNthing',
+        args: [],
+    },
+    keyval: {
+        name: 'KeyVal',
+        args: [],
+    },
+}
+module.exports = {
+    config
+};
--- index.js
+++ index.js
@@ -2,19 +2,20 @@
 const Web3 = require('web3');
 let web3 = new Web3();
 
-const config = require('./config').config;
-const deployContractUtil = require('./helper/deploy-contract');
 const currentNode = process.env.currentNode;
+const config = require('./config').config;
+const contractConfig = require('./contract-config').config;
+
+const deployContractUtil = require('./helper/deploy-contract');
 
 console.log('Connecting to ' + currentNode);
 web3 = new Web3(new Web3.providers.HttpProvider(config[currentNode].url));
 web3.isConnected() ? console.log('Web3 is connected to ' + config[currentNode].url) : console.log('Web3 connection failed ' + config[currentNode].url);
 
-let DoNothingContract;
-const contractName ='KeyVal';
-const args=[];
-deployContractUtil.deployContract(web3, contractName, args).then(contractInstance => {
-    DoNothingContract = contractInstance;
+let keyvalContract = contractConfig.keyval;
+
+deployContractUtil.deployContract(web3, keyvalContract.name, keyvalContract.args).then(contractInstance => {
+    keyvalContract.instance = contractInstance;
 }).catch(err => {
     console.log(err.toString());
 });
